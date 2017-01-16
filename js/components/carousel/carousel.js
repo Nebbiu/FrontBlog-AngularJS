432d4aa43b90006ab348468b8c3474f7159e4410
apprenti <nans.mourgues@hotmail.fr>
2017-01-16 11:00:18 +0100

    Add component and test it

---
+++ js/components/carousel/carousel.js
@@ -0,0 +1,7 @@
+((app) => {
+    'use strict'
+    app.component('mycarousel', {
+        templateUrl: 'js/components/carousel/carousel.html',
+
+    })
+})(angular.module('app.carousel'))
---
+++ js/components/postList/postList.js
@@ -0,0 +1,6 @@
+((app) => {
+    'use strict'
+    app.component('postList', {
+        templateUrl: 'js/components/postList/postList.html'
+})
+})(angular.module('app.postList'))
