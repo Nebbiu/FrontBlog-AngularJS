4f53d2bd83c885d26fde5f1150539163865f345f
apprenti <nans.mourgues@hotmail.fr>
2017-01-16 12:30:33 +0100

    Post function and html without style

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
 ((app) => {
 
+  app.factory('postFactory', function(){
+    return {
+      posts: [{
+              title: "Un peu de chat",
+              author: "Garfield",
+              createdAt: "1477409698997",
+              body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              image: "http://placekitten.com/600/300"
+          }, {
+              title: "Des chats et encore des chats",
+              author: "John",
+              createdAt: "1477409698997",
+              body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              image: "http://placekitten.com/600/300"
+          }, {
+              title: "Cat'astrophe",
+              author: "Bertrand",
+              createdAt: "1477409698989",
+              body: " Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              image: "http://placekitten.com/600/300"
+          }, {
+              title: "Whauuuu",
+              author: "Snow",
+              createdAt: "1477409698921",
+              body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              image: "http://placekitten.com/600/300"
+          }]
+    }
+  })
+
 })(angular.module('app', ['app.postList', 'app.carousel']));
--- js/components/postList/postList.js
+++ js/components/postList/postList.js
@@ -1,6 +1,33 @@
 ((app) => {
     'use strict'
     app.component('postList', {
-        templateUrl: 'js/components/postList/postList.html'
-})
+        templateUrl: 'js/components/postList/postList.html',
+        controller: ['$http', 'postFactory', function($http, postFactory) {
+
+            angular.extend(this, {
+                posts: [],
+                $onInit() {
+                    this.posts = postFactory.posts
+                },
+                savePost() {
+                    this.edit = false;
+                    if (!this.postSelected.createdAt) {
+
+                        this.postSelected.createdAt = Date.now();
+                        this.posts.unshift(this.postSelected);
+                        this.postSelected = null;
+                    } else {
+
+                        this.posts.forEach((post) => {
+                            if (post.createdAt === this.postSelected.createdAt) {
+                                post = this.postSelected
+                                return
+                            }
+                        })
+                    }
+                }
+            })
+        }]
+
+    })
 })(angular.module('app.postList'))
